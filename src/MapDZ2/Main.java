a7b6e4db07be2360b213449f407a373462c775d3
Vereshchagin_Mikhail <farasers@mail.ru>
2022-11-27 23:00:29 +0300

    ДЗ Map 2.1 Значение из массива интов в сумму интов

---
+++ src/MapDZ2/Main.java
@@ -0,0 +1,9 @@
+package MapDZ2;
+
+public class Main {
+    public static void main(String[] args) {
+        First first = new First();
+        System.out.println(first.generateInitialCollection());
+        System.out.println(first.changeCollection());
+    }
+}
